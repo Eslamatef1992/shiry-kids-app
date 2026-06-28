864c1a436bc4431b882d23186c8f3c47d067f79c
rokia <rokiaahmed42@gmaail.com>
2026-06-28 15:23:07 +0300

    share qr code done

--- lib/widgets/show_img_dialog.dart
+++ lib/widgets/show_img_dialog.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:shiry_kids_app/theme/app_colors.dart';
 
 import 'network_image.dart';
 
@@ -7,14 +13,34 @@ class ShowImgDialog extends StatelessWidget {
   final List<String> images;
   final bool isCoupon;
 
+  Future<void> shareImage(String imageUrl) async {
+    try {
+      final tempDir = await getTemporaryDirectory();
+      final file = File('${tempDir.path}/shared_image.png');
+      debugPrint(imageUrl);
+      final response = await Dio().get(
+        imageUrl,
+        options: Options(responseType: ResponseType.bytes),
+      );
 
+      await file.writeAsBytes(response.data);
+
+      await SharePlus.instance.share(
+        ShareParams(
+          files: [XFile(file.path)],
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
       backgroundColor: Colors.white,
       child: SizedBox(
-        width: 200,
+        width: 300,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -35,12 +61,30 @@ class ShowImgDialog extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: images.map((img) => Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: isCoupon
-                          ? Image.network('https://back.sherykids.com/$img', width: 220, height: 220, fit: BoxFit.contain)
-                          : smartImage(img, width: 220, height: 220, fit: BoxFit.contain),
+                    padding: const EdgeInsets.only(bottom: 20,),
+                    child: Container(
+                      padding: const EdgeInsets.only(bottom: 20,),
+                      decoration: const BoxDecoration(
+                        border: Border(bottom: BorderSide(width: 1.5)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: ()async{
+                             await shareImage(img);
+                            },
+                            child: const Icon(Icons.share,color: AppColors.primary,),
+
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: isCoupon
+                                ? Image.network('https://back.sherykids.com$img', width: 220, height: 220, fit: BoxFit.contain)
+                                : smartImage(img, width: 220, height: 220, fit: BoxFit.contain),
+                          ),
+                        ],
+                      ),
                     ),
                   )).toList(),
                 ),
